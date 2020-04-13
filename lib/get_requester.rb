2007f71fe464de7e3dcfe07543c387ae98b5021e
matthew enbar <matthew.enbar@gmail.com>
2020-04-13 19:11:50 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -17,10 +17,7 @@ class GetRequester
   end
 
   def parse_json
-    programs = JSON.parse(self.get_response_body)
- programs.collect do |program|
-   program["agency"]
- end
+    hash = JSON.parse(self.get_response_body)
   end
 
 end
