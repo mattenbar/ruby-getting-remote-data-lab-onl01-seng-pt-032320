cc4a5247574cabed855f5c219e01abea7baa8ebf
matthew enbar <matthew.enbar@gmail.com>
2020-04-13 19:12:09 +0000

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,23 @@
+require 'net/http'
+require 'open-uri'
+require 'json'
 # Write your code here
+class GetRequester
+
+  attr_accessor :url
+
+  def initialize(url)
+    @url = url
+  end
+
+  def get_response_body
+    uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+  end
+
+  def parse_json
+    hash = JSON.parse(self.get_response_body)
+  end
+
+end
